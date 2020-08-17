49ff38ec0244aa124bcd571a3d349a4b7757b3df
Wolfgang Kritzinger <wkritzinger@atlassian.com>
2020-08-17 16:17:38 +1000

    Compress transferred logs

--- stream-logs.sh
+++ stream-logs.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 usage() {
-    echo "Usage: $0 host:file [...host:file]" >&2
-    if [[ -n "$1" ]]; then
-        echo "   Error: $1"
-    fi
-    exit 1
+	echo "Usage: $0 host:file [...host:file]" >&2
+	if [[ -n "$1" ]]; then
+		echo "   Error: $1"
+	fi
+	exit 1
 }
 if [ $# -le 0 ]; then
-    usage
+	usage
 fi
 
 ARGS=()
@@ -16,26 +16,26 @@ ARGS=()
 set -euo pipefail
 
 while [ $# -gt 0 ]; do
-    if [[ "$1" != *:* ]]; then
-        usage "Unable to parse argument: $1"
-    fi
-    ARGS+=("$1")
-    shift
+	if [[ "$1" != *:* ]]; then
+		usage "Unable to parse argument: $1"
+	fi
+	ARGS+=("$1")
+	shift
 done
 
 run() {
-    set -euo pipefail
-    local remote="$1"
-    local host="${remote%%:*}"
-    local file="${remote#*:}"
-    local base
-    local size
-    base="$(basename "$file")"
-    local target="${host}_${base}"
-    rsync -P --no-whole-file "$remote" "$target"
-    size="$(wc -c "$target" | awk '{print $1}')"
-    echo "Streaming $target..."
-    ssh "$host" tail -c "+$size" -f "$file" >> "$target"
+	set -euo pipefail
+	local remote="$1"
+	local host="${remote%%:*}"
+	local file="${remote#*:}"
+	local base
+	local size
+	base="$(basename "$file")"
+	local target="${host}_${base}"
+	rsync -z -P --no-whole-file "$remote" "$target"
+	size="$(wc -c "$target" | awk '{print $1}')"
+	echo "Streaming $target..."
+	ssh "$host" "tail -c \"+$size\" -f \"$file\" | gzip -c" | gzip -d >> "$target"
 }
 
 export -f run
